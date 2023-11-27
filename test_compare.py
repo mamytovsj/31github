03a52efd1836059615ec44a39b5ac4ca8a917760
Tolgonai Mamytova <122642257+mamytovsj@users.noreply.github.com>
2023-11-28 00:30:05 +0600

    Add files via upload

---
+++ test_compare.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.mark.great
+def test_greater():
+    num = 100
+    assert num > 100
+
+
+@pytest.mark.great
+def test_greater_equal():
+    num = 100
+    assert num >= 100
+
+@pytest.mark.other
+def test_less():
+    num = 100
+    assert num < 200
