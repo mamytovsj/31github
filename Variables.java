f54b6bed04c0f032240c4856cd8613e13b660132
Tolgonai Mamytova <122642257+mamytovsj@users.noreply.github.com>
2023-12-05 11:51:44 +0600

    Variables.java

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Объявление и инициализация переменных разных типов данных
+        int age = 25; // целочисленная переменная
+        double height = 1.75; // переменная с плавающей точкой
+        char gender = 'M'; // символьная переменная
+        boolean isStudent = true; // логическая переменная
+        String name = "John"; // строковая переменная
+
+        // Вывод информации, содержащей переменные
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Рост: " + height + " м");
+        System.out.println("Пол: " + gender);
+        System.out.println("Студент: " + isStudent);
+    }
+}
